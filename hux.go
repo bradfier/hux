b7e267bbb977989e36a863627af0a3279918f0c1
Richard Bradfield <bradfier@fstab.me>
2016-12-15 22:01:17 +0000

    Add GetArrivals, fix visibility and refactor doRequest logic

--- hux.go
+++ hux.go
@@ -6,21 +6,35 @@ import (
 	"net/http"
 )
 
-type hux struct {
+type Hux struct {
 	baseURI     string
 	accessToken string
 }
 
-func NewHux(baseURI, accessToken string) *hux {
-	return &hux{baseURI, accessToken}
+func NewHux(baseURI, accessToken string) *Hux {
+	return &Hux{baseURI, accessToken}
 }
 
-func (hux *hux) sendRequest(uri string) (resp *http.Response, err error) {
+func (hux *Hux) sendRequest(uri string) (resp *http.Response, err error) {
 	uri = fmt.Sprintf("%s%s?accessToken=%s", hux.baseURI, uri, hux.accessToken)
 	return http.Get(uri)
 }
 
-func (hux *hux) GetCRSCodes(filter string) (stationCodes *[]CRSStationCode, err error) {
+func (hux *Hux) doRequest(uri string, data interface{}) error {
+	resp, err := hux.sendRequest(uri)
+
+	if err != nil {
+		return err
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(data); err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (hux *Hux) GetCRSCodes(filter string) (stationCodes *[]CRSStationCode, err error) {
 	uri := "/crs/" + filter
 	resp, err := hux.sendRequest(uri)
 
@@ -35,20 +49,20 @@ func (hux *hux) GetCRSCodes(filter string) (stationCodes *[]CRSStationCode, err 
 	return stationCodes, err
 }
 
-func (hux *hux) GetAllCRSCodes() (stationCodes *[]CRSStationCode, err error) {
+func (hux *Hux) GetAllCRSCodes() (stationCodes *[]CRSStationCode, err error) {
 	return hux.GetCRSCodes("")
 }
 
-func (hux *hux) GetDepartures(hq huxQuery) (ts *boardResponse, err error) {
+func (hux *Hux) GetDepartures(hq huxQuery) (ts *boardResponse, err error) {
+	ts = new(boardResponse)
 	uri := fmt.Sprintf("/departures/%s", hq)
+	err = hux.doRequest(uri, ts)
+	return
+}
 
-	resp, err := hux.sendRequest(uri)
-
-	if err != nil {
-		return
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&ts)
-
-	return ts, err
+func (hux *Hux) GetArrivals(hq huxQuery) (ts *boardResponse, err error) {
+	ts = new(boardResponse)
+	uri := fmt.Sprintf("/arrivals/%s", hq)
+	err = hux.doRequest(uri, ts)
+	return
 }
